e7b13bebe2b4094076610d9d7c74e967b01be0e2
Preethi Davupati <preethidavupatii@gmail.com>
2026-04-08 09:30:38 +0530

    app.py

--- app.py
+++ app.py
@@ -227,5 +227,8 @@ def get_history():
     return jsonify(history)
 
 # 🚀 Run
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
